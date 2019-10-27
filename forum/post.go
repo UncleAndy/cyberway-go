03b9eff38328b83cc7d1142f0171549394530f68
Andrey Velikoredchanin <andy@andyhost.ru>
2019-10-27 12:47:11 +0300

    Исправление структуры сообщения в соответствии с данными abi

--- forum/post.go
+++ forum/post.go
@@ -6,7 +6,7 @@ import (
 
 // NewMessage is an action representing a simple message to be posted
 // through the chain network.
-func NewMessage(msg *MessageAction) *eos.Action {
+func CreateMessage(msg *CreateMssg) *eos.Action {
 	a := &eos.Action{
 		Account: ForumAN,
 		Name:    ActN("createmssg"),
@@ -19,25 +19,38 @@ func NewMessage(msg *MessageAction) *eos.Action {
 }
 
 // Post represents the `eosio.forum::post` action.
-type MessageAction struct {
-	Id       *MessageIdType `json:"message_id"`
-	ParentId *MessageIdType `json:"parent_id"`
+type Message struct {
+	Id       		MssgId 			`json:"message_id"`
+	ParentId 		MssgId 			`json:"parent_id"`
 
-	Language string `json:"languagemssg"`
-	Header   string `json:"headermssg"`
-	Body     string `json:"bodymssg"`
+	Language 		string 			`json:"languagemssg"`
+	Header   		string 			`json:"headermssg"`
+	Body     		string 			`json:"bodymssg"`
 
-	Tags         []string `json:"tags"`
-	JsonMetadata string   `json:"jsonmetadata"`
+	Tags         	[]string 		`json:"tags"`
+	JsonMetadata 	string   		`json:"jsonmetadata"`
 
-	TokenProp     uint64      `json:"tokenprop"`
-	MaxPayout     uint64     `json:"max_payout"`
-	Beneficiaries []eos.AccountName `json:"beneficiaries"`
-	CuratorsPrcnt uint64      `json:"curators_prcnt"`
-	VestPayment   bool     `json:"vestpayment"`
+	TokenProp     	PercentT     	`json:"tokenprop"`
+	MaxPayout     	Asset     		`json:"max_payout,ommitempty"`
+	Beneficiaries 	[]Beneficiary 	`json:"beneficiaries"`
+	CuratorsPrcnt 	uint16      	`json:"curators_prcnt,ommitempty"`
+	VestPayment   	bool     		`json:"vestpayment"`
 }
 
-type MessageIdType struct {
+type CreateMssg struct {
+	Message
+}
+
+type Asset string
+
+type PercentT uint16
+
+type Beneficiary struct {
+	Account		eos.Name	`json:"account"`
+	Weight		PercentT	`json:"weight"`
+}
+
+type MssgId struct {
 	Author		eos.AccountName	`json:"author"`
 	Permlink	string			`json:"permlink"`
 }
