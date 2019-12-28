23395c8cbff8ab08bfc71e2821cc7e325ce8a765
Andrey Velikoredchanin <andy@andyhost.ru>
2019-12-28 16:58:44 +0300

    Fix open actions

--- token/open.go
+++ token/open.go
@@ -2,9 +2,24 @@ package token
 
 import eos "github.com/UncleAndy/cyberway-go"
 
-func NewOpen(creator, owner string, symbol eos.Symbol, permission string) *eos.Action {
+func NewTokenOpen(creator, owner string, symbol eos.Symbol, permission string) *eos.Action {
 	return &eos.Action{
 		Account: AN("cyber.token"),
+		Name:    ActN("open"),
+		Authorization: []eos.PermissionLevel{
+			{Actor: AN(creator), Permission: PN(permission)},
+		},
+		ActionData: eos.NewActionData(Open{
+			RamPayer: eos.AccountName(creator),
+			Owner:    eos.AccountName(owner),
+			Symbol:   symbol,
+		}),
+	}
+}
+
+func NewVestingOpen(creator, owner string, symbol eos.Symbol, permission string) *eos.Action {
+	return &eos.Action{
+		Account: AN("gls.vesting"),
 		Name:    ActN("open"),
 		Authorization: []eos.PermissionLevel{
 			{Actor: AN(creator), Permission: PN(permission)},
