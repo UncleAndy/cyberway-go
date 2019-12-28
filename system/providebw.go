47d1e18b6659a4f967c93793aa3dee6d9ede202a
Andrey Velikoredchanin <andy@andyhost.ru>
2019-12-28 20:02:44 +0300

    Add action providebw

---
+++ system/providebw.go
@@ -0,0 +1,27 @@
+package system
+
+import eos "github.com/UncleAndy/cyberway-go"
+
+func NewProvideBW(account, provider eos.AccountName, usingAccount eos.AccountName, usingPermission eos.PermissionName) *eos.Action {
+	a := &eos.Action{
+		Account: AN("cyber"),
+		Name:    ActN("providebw"),
+		Authorization: []eos.PermissionLevel{
+			{
+				Actor:      usingAccount,
+				Permission: usingPermission,
+			},
+		},
+		ActionData: eos.NewActionData(ProvideBW{
+			Account:    account,
+			Provider:	provider,
+		}),
+	}
+
+	return a
+}
+
+type ProvideBW struct {
+	Account     eos.AccountName    `json:"account"`
+	Provider    eos.AccountName    `json:"provider"`
+}
