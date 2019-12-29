59ca887e5a62117ef29d98e3614f1be0b8ef5cd1
Andrey Velikoredchanin <andy@andyhost.ru>
2019-12-29 21:36:21 +0300

    Add new operation gls.vesting@undelegate

--- token/delegate.go
+++ token/delegate.go
@@ -19,9 +19,30 @@ func NewDelegateVesting(from, to string, val eos.Asset, interest uint16, permiss
 	}
 }
 
+func NewUnDelegateVesting(from, to string, val eos.Asset, interest uint16, permission string) *eos.Action {
+	return &eos.Action{
+		Account: AN("gls.vesting"),
+		Name:    ActN("undelegate"),
+		Authorization: []eos.PermissionLevel{
+			{Actor: AN(from), Permission: PN(permission)},
+		},
+		ActionData: eos.NewActionData(UnDelegate{
+			From: AN(from),
+			To: AN(to),
+			Quantity: val,
+		}),
+	}
+}
+
 type Delegate struct {
 	From			eos.AccountName		`json:"from"`
 	To				eos.AccountName		`json:"to"`
 	Quantity		eos.Asset			`json:"quantity"`
 	InterestRate	uint16			`json:"interest_rate"`
+}
+
+type UnDelegate struct {
+	From			eos.AccountName		`json:"from"`
+	To				eos.AccountName		`json:"to"`
+	Quantity		eos.Asset			`json:"quantity"`
 }
